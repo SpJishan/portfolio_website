4cde564d117b12ee539e3be7cec99cdbfa6313d9
Shafinul Pasha <jishan523@gmail.com>
2023-10-03 17:03:28 +0600

    Completed timeline

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -38,9 +38,16 @@ export const projects = [
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
+  { year: 2010, text: 'Started my journey as Graphics Designer', },
+  { year: 2012, text: 'Designed and sold some logo , icon, cover image for 3 years', },
+  { year: 2014, text: 'Started teaching job both in as private tuitor and as mentor in coaching centre for next 6 years', },
+  { year: 2017, text: 'Started my journey as web developer', },
+  { year: 2018, text: 'Started learning After effect , Premiare pro for promotional video editing', },
+  { year: 2019, text: 'Founded JavaScript Mastery, Learning programming in web platform through React with HTML, CSS, Javascript', },
+  { year: 2020, text: 'Practiced React.js , Also starded learning professional and advanced CSS Design', },
+  { year: 2021, text: 'Building my projects and start to contribute in github', },
+  { year: 2022, text: 'Started in Office job as It manager ', },
+  { year: 2022, text: 'In Office Job at Lamda Telecom , LivinHub Technologies as a Grahics Designer ', },
+  { year: 2023, text: 'In Office Job at Nur tech as a Grahics Designer, also at Maroon Clothings as Business Manager', },
+  { year: 2023, text: 'Uploading my projects at netlify and github repo', },
 ];
