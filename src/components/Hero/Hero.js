d8a5266fb2c3ee930a7b625466ee142f3834b542
Shafinul Pasha <jishan523@gmail.com>
2023-10-03 12:38:39 +0600

    Understood how Styled element Props Work
    Completed Hero Section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,10 +4,22 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <div>
-    Hero
-  </div>
-);
+const Hero = (props) => {
+  return (
+    <Section row nopadding>
+      <LeftSection>
+        <SectionTitle main center>
+          Welcome To <br />
+          My Personal Portfolio.
+        </SectionTitle>
+        <SectionText>
+          Hello, I am Shafinul Pasha Jishan. A Front-end Web Developer. Sementic HTML, CSS, SASS, TailwindCSS, Javascript, CSS with Javascript, React are my fields of work. Feel free to ask me, If you need any front-end web development.      
+        </SectionText>
+        <Button onClick={()=> window.location= 'https://omnifood-shafinul.netlify.app'}>Learn More</Button>
+      </LeftSection>
+
+    </Section>
+  );
+};
 
 export default Hero;
