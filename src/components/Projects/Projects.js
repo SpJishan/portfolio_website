d1a50a8bd18068754a7a550c9c617c9ce484a348
Shafinul Pasha <jishan523@gmail.com>
2023-10-03 15:06:42 +0600

    Completed Project Section Learnt about arrow function Also About array map function Edited and modify Class based constant objects Applying those constant objects in Project section Edited and organize Css property in ProjectStyes.js file

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,10 +4,60 @@ import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag,
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
 
+// const projectsDemo = [{
+//   title:'Omnifood AI Based Diet Plan',
+//   description: 'HTML CSS , Nested CSS and SASS Based single page website'
+// },{
+//   title: 'Natours Travel With Comfort',
+//   description: 'HTML with TailwindCss Based single page website'
+// },{
+//   title:'Nexter All In One Flight Plan',
+//   description: 'HTML CSS , Nested CSS and SASS Based single page website'
+// },{
+//   title: 'Shortly Get Your Link Shortened',
+//   description: 'HTML with TailwindCss Based single page website'
+// }];
+
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <Section nopadding id="projects">
+    <SectionDivider />
+    <SectionTitle main>
+      Projects
+    </SectionTitle>
+
+    <GridContainer>
+      {projects.map(({title, description, image, tags, source, visit, id})=>(
+        <BlogCard key={id}>
+          <Img src={image} />
+        <TitleContent>
+          <HeaderThree title>{title}</HeaderThree>
+          <Hr />
+        </TitleContent>
+          <CardInfo>{description}</CardInfo>
+          <div>
+          <br />
+            <TitleContent>Stack</TitleContent>
+            
+            <TagList>
+              {tags.map((tag, i)=>(
+                  <Tag key={i}>{tag}</Tag>
+              ))}
+            </TagList>
+          </div>
+          <UtilityList>
+            <ExternalLinks href={source}>Visit Website</ExternalLinks>
+            <ExternalLinks href={visit}>Github Repo</ExternalLinks>
+          </UtilityList >
+        
+
+
+        </BlogCard>
+      ))}
+
+      
+    </GridContainer>
+
+  </Section>
 );
 
 export default Projects;
